ff338072dab0b326398369506aa44cbb9d7d7249
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-01 21:25:13 +0000

    [Add] Added products app, models, fixtures and customize admin

---
+++ products/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'brand',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = (
+        'sku',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=254, unique=True, null=False)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    CODE_FIELDS_SIZE = (
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+    )
+
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    brand = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=254, unique=True, null=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey("Category", null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False)
+    fields_size = models.IntegerField(default=0, choices=CODE_FIELDS_SIZE)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
