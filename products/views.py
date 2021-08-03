39559d9cd0056a49851e77209dfe597df1704357
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-03 20:07:25 +0000

    [Add] Added owl carousel on product detail page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.db.models import Q
+from random import shuffle
 
 
 def all_products(request):
@@ -21,8 +23,29 @@ def product_detail(request, category_slug, product_slug):
     product = get_object_or_404(Product, category__slug=category_slug,
                                 slug=product_slug)
 
+    complete_the_look = None
+    you_might_also_like = None
+
+    if category.name == "2_piece_suits" or category.name == "3_piece_suits" or category.name == "tuxedos":
+        queries = Q(category__name__icontains="shirts") | Q(
+                  category__name__icontains="shoes")
+
+        complete_the_look = list(Product.objects.all().filter(queries))
+        shuffle(complete_the_look)
+
+        you_might_also_like = list(Product.objects.all().filter(
+                                   category__slug=category_slug))
+
+        shuffle(you_might_also_like)
+    else:
+        you_might_also_like = list(Product.objects.all().filter(
+                                   category__slug=category_slug))
+        shuffle(you_might_also_like)
+
     context = {
         'product': product,
+        'complete_the_look': complete_the_look,
+        'you_might_also_like': you_might_also_like,
     }
 
     return render(request, "products/product_detail.html", context)
