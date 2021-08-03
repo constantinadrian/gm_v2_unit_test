f025805d85a480364998b2387a4824533f2949cf
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-03 22:23:39 +0000

    [Add] Added search functionality

--- products/views.py
+++ products/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, Category
 from django.db.models import Q
 from random import shuffle
+from django.contrib import messages
 
 
 def all_products(request):
     """" A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, "products/products.html", context)
@@ -26,7 +41,9 @@ def product_detail(request, category_slug, product_slug):
     complete_the_look = None
     you_might_also_like = None
 
-    if category.name == "2_piece_suits" or category.name == "3_piece_suits" or category.name == "tuxedos":
+    if ((category.name == "2_piece_suits") or
+            (category.name == "3_piece_suits") or
+            (category.name == "tuxedos")):
         queries = Q(category__name__icontains="shirts") | Q(
                   category__name__icontains="shoes")
 
