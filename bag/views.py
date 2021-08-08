7b0054516ac909c9a1dc48636605fc6998a01d53
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-08 21:43:16 +0000

    [Create] Created checkout app and model

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
-from products.models import Product, Category
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404,
+    reverse,
+    HttpResponse
+)
+from products.models import Product
 from django.contrib import messages
 
 
@@ -137,11 +143,16 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
