a288c7e8efe7f8d29a9eeab8a3767bb2dfd2fde4
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-09 03:03:11 +0000

    [Add] Added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,33 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+// Create a variable with the public stripe key 
+var stripe = Stripe(stripe_public_key);
+// Create an instance of stripe elements
+var elements = stripe.elements();
+// Custom style for when creating an Element
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+// Create a card element which also accepts an style argument 
+var card = elements.create('card', {style: style});
+// Mount the card element to div
+card.mount('#card-element');
