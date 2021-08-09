38f1f4c4b346a66dc139a3de482780bd4e4ddfd5
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-09 12:01:09 +0000

    [Add] Added basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,10 +5,10 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 // Create a variable with the public stripe key 
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 // Create an instance of stripe elements
 var elements = stripe.elements();
 // Custom style for when creating an Element
@@ -31,3 +31,60 @@ var style = {
 var card = elements.create('card', {style: style});
 // Mount the card element to div
 card.mount('#card-element');
+
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+                        <span class="icon" role="alert">
+                            <i class="fas fa-times"></i>
+                        </span>
+                        <span>${event.error.message}</span>
+                    `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+
+    // before call out stripe disable both the card element and the
+    // submit button to prevent multiple submissions
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+
+    // Call the confirmCardPayment method 
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+ 
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                            <span class="icon" role="alert">
+                                <i class="fas fa-times"></i>
+                            </span>
+                            <span>${result.error.message}</span>
+                        `;
+            $(errorDiv).html(html);
+
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit()
+            }
+        }
+    });
+});
