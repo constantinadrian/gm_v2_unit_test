c9969505ee528d402de2fcddb237aab1179195e1
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-11 02:10:42 +0000

    [Add] Added edit product functionality

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
      path('', views.all_products, name='products'),
      path('add_product/', views.add_product, name="add_product"),
+     path('edit_product/<int:product_id>', views.edit_product, name="edit_product"),
      path('<slug:category_slug>/<slug:product_slug>/', views.product_detail,
           name='product_detail'),
      path('<slug:category_slug>/', views.all_products,
