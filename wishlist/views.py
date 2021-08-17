87cb2c9f833cf8219e826cadd36f0301467d2703
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-17 17:06:27 +0000

    [Update] update wishlist view and template

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,51 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from django.http import Http404
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from products.models import Product
 from .models import Wishlist
 
 
 def wishlist(request):
-    """" A view to return the index page """
-    return render(request, "wishlist/wishlist.html")
+    """" A view to return the wishlist page """
+
+    context = {}
+
+    if request.user.is_authenticated:
+        wishlist = get_object_or_404(Wishlist, user=request.user)
+
+        # Pagination show 12 products per page
+        paginator = Paginator(wishlist.products.all(), 12)
+
+        page = request.GET.get('page')
+        try:
+            all_wishlist = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            all_wishlist = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            all_wishlist = paginator.page(paginator.num_pages)
+
+        # Pagination was inspired, modified and
+        # adapted to this project from from this
+        # # Credit code
+        # https://www.youtube.com/watch?v=MAIFJ3_bcCY
+        index = all_wishlist.number - 1
+        max_index = len(paginator.page_range)
+        start_index = index - 2 if index >= 2 else 0
+        end_index = index + 3 if index <= max_index - 3 else max_index
+        page_range = paginator.page_range[start_index:end_index]
+
+        context = {
+            'wishlist': all_wishlist,
+            'page_range': page_range,
+        }
+
+    template = "wishlist/wishlist.html"
+
+    return render(request, template, context)
 
 
 def add_to_wishlist(request, product_id):
