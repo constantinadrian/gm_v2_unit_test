6d5edcbcbaa4efc92c6357ee5c656ef35df13767
constantinadrian <adrianc.chiriac@yahoo.com>
2021-08-20 01:07:51 +0000

    [Change] Change environment variable for send emails

--- gentleman_mayer/settings.py
+++ gentleman_mayer/settings.py
@@ -234,6 +234,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('HOST_EMAIL_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('HOST_EMAIL_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('HOST_EMAIL_USER')
